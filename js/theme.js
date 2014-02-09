5dbac9f583059207ad8259e5bc4fa0c79292d3ac
Rub21 <rub2106@gmail.com>
2014-02-09 08:04:54 -0500

    first

---
+++ js/theme.js
@@ -0,0 +1,9 @@
+	$(document).ready(function() {
+
+		$('.collapse').on('shown.bs.collapse', function() {
+			$(this).parent().find(".glyphicon-plus").removeClass("glyphicon-plus").addClass("glyphicon-minus");
+		}).on('hidden.bs.collapse', function() {
+			$(this).parent().find(".glyphicon-minus").removeClass("glyphicon-minus").addClass("glyphicon-plus");
+		});
+
+	});
